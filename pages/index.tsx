0b7f4f6a166f6644a8c4d3621f60a4e5a2122f3a
Lawrence Dass <lawrence.dass@publicissapient.com>
2022-04-15 14:12:57 +0530

    basic nextjs setup with tailwind, styled componenents and typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import tw from "twin.macro";
+
+import styled from 'styled-components'
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Lanwrence's new portfolio site, Lawmax  👋</h1>
     <p>
       <Link href="/about">
+        <PrimaryButton>
         <a>About</a>
+        </PrimaryButton>
       </Link>
     </p>
   </Layout>
 )
+
+
+const PrimaryButton = tw.button`bg-blue-800 text-white px-6 py-2 m-6 rounded-md hover:bg-blue-600`; 
+
+const StyledButton = styled.button`
+  background-color: red;
+  padding: 1rem;
+`;
 
 export default IndexPage
